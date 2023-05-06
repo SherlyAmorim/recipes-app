74a869c913341da000b9b71c98b950c1ff0a8a19
Ramiro <ramiro.sena.eng@gmail.com>
2023-05-05 21:30:11 -0300

    feat: render Header component in required pages

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 function Drinks() {
   return (
     <div>
-      <div>Hello World!</div>
+      <Header />
       <Footer />
     </div>
   );
