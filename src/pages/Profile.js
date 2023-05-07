dd9bf5d8a7c0fdbdd7ba45d7ddef9f854b85cf4a
Guilherme Libanori <guilhermelibanori1@gmail.com>
2023-05-07 15:28:26 -0300

    Feat: added profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const history = useHistory();
+
+  const getEmail = () => {
+    const email = localStorage.getItem('user');
+    return email ? JSON.parse(email) : [];
+  };
+
+  const onButtonClick = (param) => {
+    history.push(param);
+  };
+
+  const onLogoutClick = () => {
+    localStorage.clear('user');
+    history.push('/');
+  };
+
   return (
     <div>
       <Header />
-      <div>Hello World!</div>
+      <div data-testid="profile-email">{ getEmail().email }</div>
+      <button
+        data-testid="profile-done-btn"
+        onClick={ () => onButtonClick('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        onClick={ () => onButtonClick('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        onClick={ onLogoutClick }
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
