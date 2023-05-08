98149978d47f381cd1e81ad78a7c22a17e5a4921
Emerson Marinho <Emerson_marinhoaz@hotmail.com>
2023-05-08 01:17:34 -0300

    refactor: refactoring the code and implementing the tests

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,128 +1,31 @@
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { mealApi, cockTailApi } from '../service/fetchAPI';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Recipes from './Recipes';
+import useSearch from '../hooks/useSearch';
+import SearchForm from './SearchForm';
 
 export default function SearchBar() {
-  const [searchInput, setSearchInput] = useState('');
-  const [searchInformationRadio, setSearchInformationRadio] = useState('');
-  const [recipes, setRecipes] = useState([]);
-
-  const history = useHistory();
-  const { location } = history;
-
-  const redirectToDetailsPage = (data, route, id) => {
-    if (data.length === 1) {
-      if (route === 'meals') {
-        history.push(`/meals/${data[0][id]}`);
-      } else if (route === 'drinks') {
-        history.push(`/drinks/${data[0][id]}`);
-      }
-    }
-  };
-
-  const sliceResults = (results) => {
-    const MAX_RESULTS = 12;
-    return results.slice(0, MAX_RESULTS);
-  };
-
-  const arrayHandling = (data, string) => {
-    if (data[string] === null) {
-      global.alert('Sorry, we haven\'t found any recipes for these filters.');
-      return;
-    }
-    const recipeArray = sliceResults(data[string]);
-    if (string === 'meals') {
-      redirectToDetailsPage(recipeArray, 'meals', 'idMeal');
-    } else {
-      redirectToDetailsPage(recipeArray, 'drinks', 'idDrink');
-    }
-    console.log(recipeArray);
-    setRecipes(recipeArray);
-  };
-
-  const handleSearchSubmit = async (event) => {
-    event.preventDefault();
-
-    if (searchInformationRadio === 'firstLetter' && searchInput.length > 1) {
-      global.alert('Your search must have only 1 (one) character');
-      return;
-    }
-
-    try {
-      let data = {};
-      if (location.pathname === '/meals') {
-        data = await mealApi(searchInput, searchInformationRadio);
-        arrayHandling(data, 'meals');
-      } else {
-        data = await cockTailApi(searchInput, searchInformationRadio);
-        arrayHandling(data, 'drinks');
-      }
-    } catch (error) {
-      console.error(error);
-      global.alert('An error occurred while fetching data. Please try again.');
-    }
-  };
-
+  const {
+    searchInput,
+    setSearchInput,
+    searchInformationRadio,
+    setSearchInformationRadio,
+    recipes,
+    handleSearchSubmit,
+  } = useSearch();
+  const location = useLocation();
   const imageKey = location.pathname === '/meals' ? 'strMealThumb' : 'strDrinkThumb';
   const nameKey = location.pathname === '/meals' ? 'strMeal' : 'strDrink';
 
   return (
     <>
-      <form className="SearchBar" onSubmit={ handleSearchSubmit }>
-        <input
-          type="text"
-          name="searchInput"
-          placeholder="Search"
-          data-testid="search-input"
-          value={ searchInput }
-          onChange={ ({ target }) => setSearchInput(target.value) }
-        />
-
-        <label htmlFor="ingredient-radio">
-          <input
-            type="radio"
-            name="searchInformationRadio"
-            id="ingredient-radio"
-            data-testid="ingredient-search-radio"
-            value="ingredient"
-            onChange={ ({ target }) => setSearchInformationRadio(target.value) }
-          />
-          Ingredient
-        </label>
-
-        <label htmlFor="name-radio">
-          <input
-            type="radio"
-            name="searchInformationRadio"
-            id="name-radio"
-            data-testid="name-search-radio"
-            value="name"
-            onChange={ ({ target }) => setSearchInformationRadio(target.value) }
-          />
-          Name
-        </label>
-
-        <label htmlFor="first-letter-radio">
-          <input
-            type="radio"
-            name="searchInformationRadio"
-            id="first-letter-radio"
-            data-testid="first-letter-search-radio"
-            value="firstLetter"
-            onChange={ ({ target }) => setSearchInformationRadio(target.value) }
-          />
-          First Letter
-        </label>
-
-        <button
-          type="submit"
-          data-testid="exec-search-btn"
-          disabled={ searchInformationRadio === '' }
-        >
-          Search
-        </button>
-      </form>
+      <SearchForm
+        searchInput={ searchInput }
+        setSearchInput={ setSearchInput }
+        searchInformationRadio={ searchInformationRadio }
+        setSearchInformationRadio={ setSearchInformationRadio }
+        handleSearchSubmit={ handleSearchSubmit }
+      />
       <Recipes recipes={ recipes } imageKey={ imageKey } nameKey={ nameKey } />
     </>
   );
---
+++ src/tests/SearchBar.test.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWithRouter';
+import SearchBar from '../components/SearchBar';
+
+const INPUT = 'search-input';
+const FIRST = 'first-letter-search-radio'
+
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
+describe('SearchBar', () => {
+  beforeEach(() => {
+    renderWithRouter(<SearchBar />);
+  });
+
+  it('renders search input and radio buttons', () => {
+    const searchInput = screen.getByTestId(INPUT);
+    const ingredientRadio = screen.getByTestId('ingredient-search-radio');
+    const nameRadio = screen.getByTestId('name-search-radio');
+    const firstLetterRadio = screen.getByTestId(FIRST);
+
+    expect(searchInput).toBeInTheDocument();
+    expect(ingredientRadio).toBeInTheDocument();
+    expect(nameRadio).toBeInTheDocument();
+    expect(firstLetterRadio).toBeInTheDocument();
+  });
+
+  it('updates search input value when typed', () => {
+    const searchInput = screen.getByTestId(INPUT);
+    userEvent.type(searchInput, 'chicken');
+    expect(searchInput).toHaveValue('chicken');
+  });
+
+  it('selects radio buttons correctly', () => {
+    const ingredientRadio = screen.getByTestId('ingredient-search-radio');
+    const nameRadio = screen.getByTestId('name-search-radio');
+    const firstLetterRadio = screen.getByTestId(FIRST);
+
+    userEvent.click(ingredientRadio);
+    expect(ingredientRadio).toBeChecked();
+
+    userEvent.click(nameRadio);
+    expect(nameRadio).toBeChecked();
+
+    userEvent.click(firstLetterRadio);
+    expect(firstLetterRadio).toBeChecked();
+  });
+
+  it('displays an alert when first letter search has more than one character', async () => {
+    const searchInput = screen.getByTestId(INPUT);
+    const firstLetterRadio = screen.getByTestId(FIRST);
+    const searchButton = screen.getByTestId('exec-search-btn');
+
+    userEvent.type(searchInput, 'chicken');
+    userEvent.click(firstLetterRadio);
+    const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => {});
+
+    userEvent.click(searchButton);
+
+    await waitFor(() => {
+      expect(alertSpy).toHaveBeenCalledWith('Your search must have only 1 (one) character');
+    });
+
+    alertSpy.mockRestore();
+  });
+});
