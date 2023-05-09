dc72cf29b5e6aeecc49fd64cb37a71b9668fcfe5
Ramiro <ramiro.sena.eng@gmail.com>
2023-05-09 00:17:17 -0300

    fix: remove carousel and create scroll

--- src/components/Recomendations.jsx
+++ src/components/Recomendations.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Carousel, Card } from 'react-bootstrap';
+// import { Card } from 'react-bootstrap';
 import { fetchRecipe } from '../service/fetchAPI';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -11,7 +11,7 @@ export default function Recomendations() {
   const { location: { pathname } } = history;
   const [recipes, setRecipes] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [activeIndex, setActiveIndex] = useState(0);
+  // const [activeIndex, setActiveIndex] = useState(0);
 
   useEffect(() => {
     const type = pathname.split('/')[1];
@@ -25,7 +25,6 @@ export default function Recomendations() {
       console.log(result);
       setRecipes(Object.values(result)[0].slice(0, NUMBER_OF_RECOMMENDATIONS));
       setLoading(false);
-      setActiveIndex(0);
     };
     fetchData();
   }, [pathname]);
@@ -35,61 +34,49 @@ export default function Recomendations() {
       {loading
         ? <>Loading...</>
         : (
+
           <div>
-
             <h2>Recomendations</h2>
-            <div className="d-flex">
-
-              <Carousel
-                className="d-block w-100 overflow-hidden"
-                interval={ null }
-                onSelect={ (selectedIndex) => { setActiveIndex(selectedIndex); } }
+            <div className="d-flex flex-nowrap">
+              <ul
+                className="list-group list-group-horizontal overflow-x-scroll"
               >
-                {[0, 1, 2].map((item) => (
-                  <Carousel.Item
-                    key={ item }
-                    className="d-flex w-100"
-                  >
-                    {
-                      recipes.slice(activeIndex * 2, activeIndex * 2 + 2)
-                        .map((recipe, index) => {
-                          // const id = recipe.idDrink || recipe.idMeal;
-                          // const type = recipe.idDrink ? 'drinks' : 'meals';
-                          const title = recipe.strDrink || recipe.strMeal;
-                          const photo = recipe.strMealThumb || recipe.strDrinkThumb;
-                          return (
-                            <Card
-                              // onClick={ () => { history.push(`/${type}/${id}`); } }
-                              data-testid={
-                                `${item * 2 + index}-recommendation-card`
-                              }
-                              key={ index }
-                              className={
-                                `d-${
-                                  item === activeIndex ? 'flex' : 'none'} flex-row w-50`
-                              }
-                            >
+                {
+                  recipes.map((recipe, index) => {
+                    // const id = recipe.idDrink || recipe.idMeal;
+                    // const type = recipe.idDrink ? 'drinks' : 'meals';
+                    const title = recipe.strDrink || recipe.strMeal;
+                    const photo = recipe.strMealThumb || recipe.strDrinkThumb;
+                    return (
+                      <li
+                        // onClick={ () => { history.push(`/${type}/${id}`); } }
+                        data-testid={
+                          `${index}-recommendation-card`
+                        }
+                        key={ index }
+                        className="list-group-item m-0 p-0"
+                      >
+                        <div style={ { width: '50vw' } }>
 
-                              <img
-                                src={ photo }
-                                alt={ title }
-                                className="w-100"
-                              />
+                          <img
+                            src={ photo }
+                            alt={ title }
+                            width="100%"
+                          />
 
-                              <Carousel.Caption
-                                data-testid={
-                                  `${item * 2 + index}-recommendation-title`
-                                }
-                                className={ item === activeIndex ? 'd-flex' : 'd-none' }
-                              >
-                                {title}
-                              </Carousel.Caption>
-                            </Card>
-                          );
-                        })
-                    }
-                  </Carousel.Item>))}
-              </Carousel>
+                          <span
+                            data-testid={
+                              `${index}-recommendation-title`
+                            }
+                          >
+                            {title}
+                          </span>
+                        </div>
+                      </li>
+                    );
+                  })
+                }
+              </ul>
             </div>
           </div>)}
 
