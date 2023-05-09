46b3af752c5302e0c530c93766b854fdc389eadf
Ramiro <ramiro.sena.eng@gmail.com>
2023-05-09 03:01:00 -0300

    feat: hide startRecipe button if  recipe is done

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory, Link, useParams } from 'react-router-dom';
 import MealDetails from '../components/MealDetails';
 import DrinkDetails from '../components/DrinkDetails';
 import Recomendations from '../components/Recomendations';
+import { isRecipeDone } from '../service/localStorage/doneRecipes';
 
 function RecipeDetails() {
   const history = useHistory();
   const { location: { pathname } } = history;
+  const { id } = useParams();
 
   return (
     <div>
-      {pathname.includes('/meals')
+      {pathname.split('/')[1] === 'meals'
         ? <MealDetails />
         : <DrinkDetails />}
-      <Link
+      {!isRecipeDone(id, pathname.split('/')[1] === 'meals' ? 'meal' : 'drink')
+      && (
+        <Link
         // onClick={ history.push(`${pathname}in-progress`) }
-        className="btn btn-primary fixed-bottom"
-        data-testid="start-recipe-btn"
-        to={ `${pathname}/in-progress` }
-      >
-        Start Recipe
-      </Link>
+          className="btn btn-primary fixed-bottom"
+          data-testid="start-recipe-btn"
+          to={ `${pathname}/in-progress` }
+        >
+          Start Recipe
+        </Link>)}
       <Recomendations />
     </div>
   );
