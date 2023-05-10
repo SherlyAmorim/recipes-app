09391216d9256e9db6ce209345e8fecf41984a57
SherlyAmorim <sherlyamorim@gmail.com>
2023-05-10 18:51:23 -0300

    Feat: Created meals and drinks specification cards and filter buttons for Done Recipes page

--- src/components/DoneRecipesCard.jsx
+++ src/components/DoneRecipesCard.jsx
@@ -29,48 +29,72 @@ const doneRecipesMock = [
 
 function DoneRecipesElements() {
   const [doneRecipes, setDoneRecipes] = useState([]);
+  const [typeRecipes, setTypeRecipes] = useState([]);
 
   localStorage.setItem('doneRecipesMock', JSON.stringify(doneRecipesMock));
 
   useEffect(() => {
-    const recipesFromLocalStorage = JSON.parse(localStorage.getItem('doneRecipes'))
-     || doneRecipesMock;
-
-    setDoneRecipes(recipesFromLocalStorage);
+    const recipesFromLocalStorage = JSON.parse(localStorage.getItem('doneRecipesMock'));
+    //  || doneRecipesMock;
+    if (recipesFromLocalStorage) {
+      setDoneRecipes(recipesFromLocalStorage);
+      setTypeRecipes(recipesFromLocalStorage);
+    }
   }, []);
+
+  const filterDoneRecipes = (type) => {
+    if (type === 'all') {
+      setTypeRecipes(doneRecipes);
+    } else {
+      const recipeType = type === 'meals' ? 'meal' : 'drink';
+      const filtered = doneRecipes.filter((recipe) => recipe.type === recipeType);
+      setTypeRecipes(filtered);
+      console.log(typeRecipes);
+    }
+  };
 
   return (
     <>
+      {/* {filterDoneRecipes()} */}
+      {
+        console.log(typeRecipes)
+      }
       <form>
         <button
+          type="button"
           data-testid="filter-by-all-btn"
+          onClick={ () => filterDoneRecipes('all') }
         >
           All
         </button>
         <button
+          type="button"
           data-testid="filter-by-meal-btn"
+          onClick={ () => filterDoneRecipes('meals') }
         >
           Meals
         </button>
         <button
+          type="button"
           data-testid="filter-by-drink-btn"
+          onClick={ () => filterDoneRecipes('drinks') }
         >
           Drinks
         </button>
         <br />
       </form>
       {
-        doneRecipes?.map((meal, index) => (
+        typeRecipes?.map((recipe, index) => (
           <Card key={ index }>
             <img
               data-testid={ `${index}-horizontal-image` }
-              src={ meal.image }
+              src={ recipe.image }
               alt="imagem da receita"
               height="120"
               width="200"
             />
             <h3 data-testid={ `${index}-horizontal-name` }>
-              { `${meal.name}` }
+              { `${recipe.name}` }
             </h3>
             <button
               data-testid={ `${index}-horizontal-share-btn` }
@@ -83,14 +107,26 @@ function DoneRecipesElements() {
                 style={ { height: '35px', width: '35px' } }
               />
             </button>
-            <p data-testid={ `${index}-horizontal-top-text` }>
-              { `${meal.nationality} - ${meal.category}` }
-            </p>
+            {
+              typeRecipes[index].type === 'meal'
+                && <p
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  { `${recipe.nationality} - ${recipe.category}` }
+                </p>
+            }
+            {
+              typeRecipes[index].type === 'drink'
+                  && <p data-testid={ `${index}-horizontal-top-text` }>
+                    {`${recipe.alcoholicOrNot}`}
+                  </p>
+            }
             <p data-testid={ `${index}-horizontal-done-date` }>
-              { `Done in: ${meal.doneDate}` }
+              { `Done in: ${recipe.doneDate}` }
             </p>
             {
-              meal.tags.slice(0, 2).map((tag) => (
+              typeRecipes[index].type === 'meal'
+              && recipe.tags.slice(0, 2).map((tag) => (
                 <p
                   key={ tag }
                   data-testid={ `${index}-${tag}-horizontal-tag` }
@@ -101,47 +137,6 @@ function DoneRecipesElements() {
           </Card>
         ))
       }
-      {/* {
-        doneRecipes?.filter((x) => x.type.includes('drink')).map((meal, index) => (
-          <Card key={ index }>
-            <img
-              data-testid={ `${index}-horizontal-image` }
-              src={ meal.image }
-              alt="imagem da receita"
-              height="120"
-              width="200"
-            />
-            <h3 data-testid={ `${index}-horizontal-name` }>
-              { `${meal.name}` }
-            </h3>
-            <button
-              data-testid={ `${index}-horizontal-share-btn` }
-              style={ { height: '40px', width: '50px' } }
-            >
-              <img
-                src={ shareIcon }
-                alt="Icone de Compartilhamento da receita"
-                style={ { height: '35px', width: '35px' } }
-              />
-            </button>
-            <p data-testid={ `${index}-horizontal-top-text` }>
-              { `${meal.category}` }
-            </p>
-            <p data-testid={ `${index}-horizontal-done-date` }>
-              { `Done in: ${meal.doneDate}` }
-            </p>
-            {
-              meal.tags.slice(0, 2).map((tag) => (
-                <p
-                  key={ tag }
-                  data-testid={ `${index}-${tag}-horizontal-tag` }
-                >
-                  { `${tag}` }
-                </p>))
-            }
-          </Card>
-        ))
-      } */}
     </>
   );
 }
