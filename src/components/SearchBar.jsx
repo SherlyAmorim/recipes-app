69229a13d8171e27579ddde5cbd0b4b90c6e0708
Ramiro <ramiro.sena.eng@gmail.com>
2023-05-11 16:42:38 -0300

    fix: render cards properly

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,4 @@
 import React, { useContext, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import Recipes from './Recipes';
 import useSearch from '../hooks/useSearch';
 import SearchForm from './SearchForm';
 import context from '../contexts/MyContext';
@@ -15,25 +13,21 @@ export default function SearchBar() {
     handleSearchSubmit,
   } = useSearch();
   const { setRecipesList } = useContext(context);
-  const location = useLocation();
-  const imageKey = location.pathname === '/meals' ? 'strMealThumb' : 'strDrinkThumb';
-  const nameKey = location.pathname === '/meals' ? 'strMeal' : 'strDrink';
 
   useEffect(() => {
-    setRecipesList(recipes);
-    console.log('sending recipes to contextAPI');
+    if (recipes.length !== 0) {
+      setRecipesList(recipes);
+    }
+    console.log('recipes', recipes);
   }, [setRecipesList, recipes]);
 
   return (
-    <>
-      <SearchForm
-        searchInput={ searchInput }
-        setSearchInput={ setSearchInput }
-        searchInformationRadio={ searchInformationRadio }
-        setSearchInformationRadio={ setSearchInformationRadio }
-        handleSearchSubmit={ handleSearchSubmit }
-      />
-      <Recipes recipes={ recipes } imageKey={ imageKey } nameKey={ nameKey } />
-    </>
+    <SearchForm
+      searchInput={ searchInput }
+      setSearchInput={ setSearchInput }
+      searchInformationRadio={ searchInformationRadio }
+      setSearchInformationRadio={ setSearchInformationRadio }
+      handleSearchSubmit={ handleSearchSubmit }
+    />
   );
 }
